fd954793ad9b74c491a6150d41a9d870a0c7f077
Daniel Holmberg <daniel.holmberg97@gmail.com>
2019-06-30 21:16:50 +0300

    Backend relative url and react proxy

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+//const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
-}  
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
 
 const create = newObject => {
   const request = axios.post(baseUrl, newObject)
@@ -17,8 +18,8 @@ const update = (id, newObject) => {
 }
 
 const remove = (id) => {
-    const request = axios.delete(`${baseUrl}/${id}`)
-    return request.then(response => response.data)
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then(response => response.data)
 }
 
 export default { getAll, create, update, remove }
