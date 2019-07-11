1c1cb86240637d629c506b633bfdd2f3048ea2f8
Daniel Holmberg <daniel.holmberg97@gmail.com>
2019-07-11 23:51:34 +0300

    Part 4 helper functions and unit tests step 2

--- part4/bloglist/.eslintrc.js
+++ part4/bloglist/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     'env': {
         'commonjs': true,
         'es6': true,
-        'node': true
+        'node': true,
+        'jest': true,
     },
     'extends': 'eslint:recommended',
     'globals': {
@@ -15,11 +16,11 @@ module.exports = {
     'rules': {
         'indent': [
             'error',
-            2
+            4
         ],
         'linebreak-style': [
             'error',
-            'unix'
+            'windows'
         ],
         'quotes': [
             'error',
