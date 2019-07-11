a07617e488c1fdb2c43a92e8584e4eeaa647e5fa
Daniel Holmberg <daniel.holmberg97@gmail.com>
2019-07-11 22:49:19 +0300

    Part 4 blog list step 2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,33 +1,9 @@
-require('dotenv').config()
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const Blog = require('./models/blog')
+const config = require('./utils/config')
 
-app.use(cors())
-app.use(bodyParser.json())
+const server = http.createServer(app)
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
 })
