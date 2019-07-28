d9c3b87d5c366c73c498cbefd132be32be61e1c7
Daniel Holmberg <daniel.holmberg97@gmail.com>
2019-07-28 21:07:00 +0300

    Cleaner code

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -13,44 +13,55 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-    const body = request.body
+    const blog = new Blog(request.body)
+
+    if (!request.token) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
 
     const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-    if (!request.token || !decodedToken.id) {
+    if (!decodedToken.id) {
         return response.status(401).json({ error: 'token missing or invalid' })
     }
 
     const user = await User.findById(decodedToken.id)
 
-    const blog = new Blog({ ...body, user: user._id })
-    if (blog.likes === undefined) blog.likes = 0
-    if (blog.title === undefined && blog.url === undefined) {
-        response.status(400).end()
-    } else {
-        const savedBlog = await blog.save()
-        user.blogs = user.blogs.concat(savedBlog._id)
-        await user.save()
-        response.status(201).json(savedBlog)
+    blog.user = user.id
+
+    if (!blog.url || !blog.title) {
+        return response.status(400).send({ error: 'title or url missing' }).end()
     }
+
+    if (!blog.likes) {
+        blog.likes = 0
+    }
+
+    const result = await blog.save()
+    user.blogs = user.blogs.concat(blog)
+    await user.save()
+
+    response.status(201).json(result)
 })
 
-blogsRouter.delete('/:id', async (request, response, next) => {
-    try {
-        const decodedToken = jwt.verify(request.token, process.env.SECRET)
+blogsRouter.delete('/:id', async (request, response) => {
+    if (!request.token) {
+        return response.status(401).json({ error: 'token missing' })
+    }
 
-        if (!request.token || !decodedToken.id) {
-            return response.status(401).json({ error: 'token missing or invalid' })
-        }
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-        if (decodedToken.id.toString() !== blog.user.toString()) {
-            return response.status(401).json({ error: 'unauthorized deletion attempt' })
-        }
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
 
+    const blog = await Blog.findById(request.params.id)
+
+    if (blog.user.toString() === decodedToken.id) {
         await Blog.findByIdAndRemove(request.params.id)
         response.status(204).end()
-    } catch (exception) {
-        next(exception)
+    } else {
+        response.status(404).end()
     }
 })
 
